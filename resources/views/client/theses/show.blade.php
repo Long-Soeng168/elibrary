7aedf71f2afd5aacf8f6c3651f315874cfc2f659
Long-Soeng168 <generalpurpose167@gmail.com>
2024-07-11 18:41:20 +0700

    update

--- resources/views/client/theses/show.blade.php
+++ resources/views/client/theses/show.blade.php
@@ -162,6 +162,16 @@
                             </p>
                         </div>
                     @endif
+                    @if ($item->student_name)
+                        <div class="flex nowrap">
+                            <p class="w-[150px] uppercase tracking-wide text-sm text-gray-500 dark:text-gray-300 font-semibold border-r border-gray-600 dark:border-gray-300 pr-5 mr-5">
+                                {{ __('messages.student') }}
+                            </p>
+                            <p class="text-sm text-gray-600 dark:text-gray-200">
+                                {{ $item->student_name }}
+                            </p>
+                        </div>
+                    @endif
                     @if ($item->lecturer?->name)
                         <div class="flex nowrap">
                             <p class="w-[150px] uppercase tracking-wide text-sm text-gray-500 dark:text-gray-300 font-semibold border-r border-gray-600 dark:border-gray-300 pr-5 mr-5">
