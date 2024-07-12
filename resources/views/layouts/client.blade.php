c8c544214b0a76bb90553979441edf4ce5971a58
Long-Soeng168 <generalpurpose167@gmail.com>
2024-07-12 11:59:59 +0700

    update

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Ebooks</title>
+    <title>{{ $websiteInfo->name }}</title>
 
     <!-- Start CSS -->
     <link rel="stylesheet" href="{{ asset('assets/css/swiper.css') }}" />
