8e8206b65327beb388eb6a6eccb49a3cd80021fa
Long-Soeng168 <generalpurpose167@gmail.com>
2024-07-12 14:07:57 +0700

    update khmer in index each archive

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -282,9 +282,16 @@
 
                             <a wire:key="{{ $item->id }}-{{ $index }}" class="block group" href="{{ url('articles/'.$item->id) }}">
                                 <div class="w-full overflow-hidden bg-gray-100 border rounded-md dark:bg-neutral-800">
-                                    <img class="w-full aspect-[6/9] group-hover:scale-110 transition-transform duration-500 ease-in-out object-cover rounded-md"
+                                    @if ($item->image)
+                                        <img class="w-full aspect-[6/9] group-hover:scale-110 transition-transform duration-500 ease-in-out object-cover rounded-md"
                                         src="{{ asset('assets/images/articles/thumb/'.$item->image) }}"
                                         alt="Image Description" />
+                                    @else
+                                        <img class="w-full aspect-[6/9] p-6 group-hover:scale-110 transition-transform duration-500 dark:bg-gray-300 ease-in-out object-contain rounded-md"
+                                        src="{{ asset('assets/icons/no-image.png') }}"
+                                        alt="Image Description" />
+                                    @endif
+
                                 </div>
 
                                 <div class="relative pt-2" x-data="{ tooltipVisible: false }">
--- resources/views/livewire/publication-index.blade.php
+++ resources/views/livewire/publication-index.blade.php
@@ -282,9 +282,16 @@
 
                         <a wire:key="{{ $item->id }}-{{ $index }}" class="block group" href="{{ url('publications/'.$item->id) }}">
                             <div class="w-full overflow-hidden bg-gray-100 border rounded-md dark:bg-neutral-800">
-                                <img class="w-full aspect-[6/9] group-hover:scale-110 transition-transform duration-500 ease-in-out object-cover rounded-md"
-                                    src="{{ asset('assets/images/publications/thumb/'.$item->image) }}"
+                                @if ($item->image)
+                                        <img class="w-full aspect-[6/9] group-hover:scale-110 transition-transform duration-500 ease-in-out object-cover rounded-md"
+                                        src="{{ asset('assets/images/publications/thumb/'.$item->image) }}"
+                                        alt="Image Description" />
+                                @else
+                                    <img class="w-full aspect-[6/9] p-6 group-hover:scale-110 transition-transform duration-500 dark:bg-gray-300 ease-in-out object-contain rounded-md"
+                                    src="{{ asset('assets/icons/no-image.png') }}"
                                     alt="Image Description" />
+                                @endif
+
                             </div>
 
                             <div class="relative pt-2" x-data="{ tooltipVisible: false }">
