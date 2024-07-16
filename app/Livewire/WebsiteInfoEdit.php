c21a449adfe21c82bfda5d037433272219b25b66
Long-Soeng168 <generalpurpose167@gmail.com>
2024-07-16 16:22:46 +0700

    update

--- app/Livewire/WebsiteInfoEdit.php
+++ app/Livewire/WebsiteInfoEdit.php
@@ -83,8 +83,8 @@ class WebsiteInfoEdit extends Component
             if (File::exists($old_path)) {
                 File::delete($old_path);
             }
-            // $filename = time() . '_' . $this->banner->getClientOriginalName();
-            $filename = 'banner.png';
+            $filename = time() . '_' . $this->banner->getClientOriginalName();
+            // $filename = 'banner.png';
 
             $image_path = public_path('assets/images/website_infos/'.$filename);
             $imageUpload = Image::make($this->banner->getRealPath())->save($image_path);
