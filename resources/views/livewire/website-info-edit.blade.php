0cdf9d0fea30b40d36fe04ad364f34d4e975db1b
Long-Soeng168 <generalpurpose167@gmail.com>
2024-07-17 10:34:34 +0700

    update

--- resources/views/livewire/website-info-edit.blade.php
+++ resources/views/livewire/website-info-edit.blade.php
@@ -163,7 +163,7 @@
 
                 <div class="flex flex-col flex-1">
                     <label class='mb-4 text-sm font-medium text-gray-600 dark:text-white'>
-                        Upload Logo (Max: 2MB) <span class="text-red-500">*</span>
+                        Upload Logo (Max: 2MB) (Recommend : 1x1 or 512px x 512px) <span class="text-red-500">*</span>
                     </label>
                     <div class="relative flex items-center justify-center w-full -mt-3 overflow-hidden">
                         <label for="dropzone-file"
