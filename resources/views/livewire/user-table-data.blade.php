a9c0b64e212ebc43a24a1d1fa8bb88ef95c5a365
Long-Soeng168 <generalpurpose167@gmail.com>
2024-07-25 13:59:52 +0700

    update

--- resources/views/livewire/user-table-data.blade.php
+++ resources/views/livewire/user-table-data.blade.php
@@ -196,7 +196,7 @@
                     <td class="px-6 py-4">
                         <div class="flex items-start justify-center gap-3">
                             @can('view user')
-                            <a href="#profileFrame{{ $item->id }}" class="glightbox4" data-gallery="gallery1">
+                            <a wire:key='{{ rand() }}' href="#profileFrame{{ $item->id }}" class="glightbox4" data-gallery="gallery1">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye">
                                     <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
                                     <circle cx="12" cy="12" r="3" />
