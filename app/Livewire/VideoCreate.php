d27af01099842d40f9feaa5411317679fd649906
Long-Soeng168 <generalpurpose167@gmail.com>
2024-08-08 20:12:31 +0700

    dd

--- app/Livewire/VideoCreate.php
+++ app/Livewire/VideoCreate.php
@@ -269,10 +269,6 @@ class VideoCreate extends Component
             'file' => 'file|max:20480', // 2MB Max
         ]);
 
-        if(!$this->name){
-            $this->name = $this->file->getClientOriginalName();
-        }
-
         session()->flash('success', 'file successfully uploaded!');
     }
 
