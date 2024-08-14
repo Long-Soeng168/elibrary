0c2ed0be5297d11c1b191570cf45e5ce4eb27503
Long-Soeng168 <generalpurpose167@gmail.com>
2024-08-14 14:39:24 +0700

    epub progress bar

--- resources/views/livewire/publication-create.blade.php
+++ resources/views/livewire/publication-create.blade.php
@@ -932,11 +932,13 @@
             {{-- End Image Upload --}}
 
             {{-- Start PDF Upload --}}
-            <div class="flex items-center space-4" wire:key='uploadpdf' x-data="{ uploading: false, progress: 0, paused: false }"
+            <div class="flex items-center space-4" wire:key='uploadpdf'
+                x-data="{ uploading: false, progress: 0, paused: false }"
                 x-on:livewire-upload-start="uploading = true; progress = 0; console.log('started');"
                 x-on:livewire-upload-finish="uploading = false; console.log('finished');"
                 x-on:livewire-upload-error="uploading = false"
-                x-on:livewire-upload-progress="progress = $event.detail.progress">
+                x-on:livewire-upload-progress="progress = $event.detail.progress"
+            >
                 <div class="flex flex-col flex-1">
                     <label class='mb-4 text-sm font-medium text-gray-600 dark:text-white'>
                         Upload PDF File (Max : 50MB) <span class="text-red-500">*</span>
@@ -973,6 +975,7 @@
                             Uploading...
                         </span>
                     </div>
+                    {{-- Start Progess Bar --}}
                     <style>
                         progress {
                             border-radius: 3px;
@@ -992,6 +995,7 @@
                         <span x-text="progress + '%'"></span>
                         <progress class="w-full" max="100" x-bind:value="progress"></progress>
                     </div>
+                    {{-- End Progress Bar --}}
                     <x-input-error :messages="$errors->get('pdf')" class="mt-2" />
                 </div>
             </div>
