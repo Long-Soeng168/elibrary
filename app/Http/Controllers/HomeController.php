78f61791d8c6a8187096af31bfb5dc7753e3c08f
Long-Soeng168 <generalpurpose167@gmail.com>
2024-08-15 12:22:08 +0700

    update stream file

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -255,7 +255,7 @@ class HomeController extends Controller
             ]);
         }
 
-        if (!$item->can_download && !auth()->check()) {
+        if (!$item->can_download && !$item->can_read && !auth()->check()) {
             abort(403);
         }
 
