ebb155bc273f3887af92747bd589a173c51f1d7b
Long-Soeng168 <generalpurpose167@gmail.com>
2024-08-16 14:30:41 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -52,7 +52,7 @@ class HomeController extends Controller
         $videos = Video::inRandomOrder()->limit(8)->get();
         $images = Image::inRandomOrder()->limit(8)->get();
         $audios = Audio::inRandomOrder()->limit(8)->get();
-        $bulletins = News::inRandomOrder()->limit(10)->get();
+        $bulletins = News::inRandomOrder()->limit(8)->get();
         $theses = Thesis::inRandomOrder()->limit(10)->get();
         $journals = Journal::inRandomOrder()->limit(10)->get();
         $articles = Article::inRandomOrder()->limit(10)->get();
