28ac9971466033b7f69fa839ab4ebed7eb8e2e63
root <root@production.apps>
2024-08-30 08:58:45 +0000

    update

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -13,6 +13,8 @@
             <canvas id="chartBar"></canvas>
         </div> --}}
 
+        @if(auth()->user()->hasRole('super-admin')) 
+       
         <div class="p-4">
 
             <div class="grid items-start grid-cols-3 gap-4 sm:grid-cols-3 lg:grid-cols-{{ (count($menu_databases) <= 5) ? count($menu_databases) : '6' }}">
@@ -319,6 +321,7 @@
 
                 </div>
             </div>
+             @endif
 
         </div>
         </div>
