267e823de23de776be9f1c6f9876e420187ac144
root <root@production.apps>
2024-09-03 09:38:50 +0000

    update

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -365,7 +365,7 @@
                                 </h2>
                                 <ul class="mb-8 font-medium dark:text-gray-400">
                                     <li class="mb-4">
-                                        <a href="https://info.flagcounter.com/i5nY"><img src="https://s11.flagcounter.com/count2/i5nY/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
+                                        <a href="https://info.flagcounter.com/ob8e"><img src="https://s11.flagcounter.com/count2/ob8e/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
                                     </li>
                                 </ul>
                             </div> 
